6342165a4d404dcd31514531531bfc55badd60dc
jungwoole91 <jungwoole91@gmail.com>
2022-09-15 07:09:06 +0900

    feat: tailwind 스타일 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import Layout from "../layout/Layout"
 const Home = () => {
   return (
     <Layout>
-
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </Layout>
   )
 }
